df6b7f2da0cda9b23e6f3ac7f3c73e4e64c7553f
Jeevan <153435148+jeevankma@users.noreply.github.com>
2024-06-07 17:07:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -84,7 +84,7 @@ resource "aws_iam_role" "helloWorldEcsTaskExecutionRole" {
 }
 
 resource "aws_iam_role_policy_attachment" "helloWorldEcsTaskExecutionRolePolicy" {
-  role       = aws_iam_role.helloWorldEcsTaskExecutionRole.jeevan
+  role       = aws_iam_role.helloWorldEcsTaskExecutionRole.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
